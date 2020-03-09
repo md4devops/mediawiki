d118c9c3b0bb2a465eca1a0597fffbb4b039eb11
Alex <alex.randrianan@gmail.com>
2020-03-09 15:12:28 +0100

    change pipeline 3

--- pipeline_jenkins/install_mediawiki.groovy
+++ pipeline_jenkins/install_mediawiki.groovy
@@ -5,52 +5,55 @@ pipeline {
 options {
 ansiColor("xterm")
 }
-  stage ('Install serveur LAMP') {
-    when {
-      expression { params.version != "migration" }
+
+stages {
+    stage ('Install serveur LAMP') {
+      when {
+        expression { params.version != "migration" }
+      }
+      environment {
+        ANSIBLE_FORCE_COLOR = true
+      }
+      steps {
+        ansiblePlaybook (
+          colorized: true,
+          playbook: 'install_lamp.yml',
+          inventory: 'inventories/alex/hosts',
+          extras: '${VERBOSE}'
+        )
+      }
     }
-    environment {
-      ANSIBLE_FORCE_COLOR = true
+    stage ('Deploy appli') {
+      environment {
+        ANSIBLE_FORCE_COLOR = true
+      }
+      when {
+        expression { params.version != "migration" }
+      }
+      steps {
+        ansiblePlaybook (
+          colorized: true,
+          playbook: 'install_deploy.yml',
+          inventory: 'inventories/alex/hosts',
+          extras: '${VERBOSE}'
+        )
+      }
     }
-    steps {
-      ansiblePlaybook (
-        colorized: true,
-        playbook: 'install_lamp.yml',
-        inventory: 'inventories/alex/hosts',
-        extras: '${VERBOSE}'
-      )
-    }
-  }
-  stage ('Deploy appli') {
-    environment {
-      ANSIBLE_FORCE_COLOR = true
-    }
-    when {
-      expression { params.version != "migration" }
-    }
-    steps {
-      ansiblePlaybook (
-        colorized: true,
-        playbook: 'install_deploy.yml',
-        inventory: 'inventories/alex/hosts',
-        extras: '${VERBOSE}'
-      )
-    }
-  }
-  stage ('Migration 01.01.00') {
-    when {
-      expression { params.version == "01.01.00" | params.version == 'migration' }
-    }
-    environment {
-      ANSIBLE_FORCE_COLOR = true
-    }
-    steps {
-      ansiblePlaybook (
-        colorized: true,
-        playbook: 'install_migration.yml',
-        inventory: 'inventories/alex/hosts',
-        extras: '${VERBOSE}'
-      )
+    stage ('Migration 01.01.00') {
+      when {
+        expression { params.version == "01.01.00" | params.version == 'migration' }
+      }
+      environment {
+        ANSIBLE_FORCE_COLOR = true
+      }
+      steps {
+        ansiblePlaybook (
+          colorized: true,
+          playbook: 'install_migration.yml',
+          inventory: 'inventories/alex/hosts',
+          extras: '${VERBOSE}'
+        )
+      }
     }
   }
 }
